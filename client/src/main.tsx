a48a108b3b4bf97df66288e1cc452377ad0fb30a
Shlomi Yahbes <syahbes@gmail.com>
2023-12-29 13:49:33 +0200

    style

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/pages/app";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "@/styles/theme";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ client/src/pages/app/index.tsx
@@ -0,0 +1,11 @@
+import Styles from "@/styles";
+
+const App = () => {
+  return (
+    <div>
+      <Styles isModalOpen={false} />
+      App
+    </div>
+  );
+};
+export default App;
